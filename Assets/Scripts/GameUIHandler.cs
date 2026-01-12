17eaf6e2b0f7bbc8740509433b61dfa1e2c27f2b
Tomasz Szyndler <tsszyndler@gmail.com>
2026-01-12 11:44:50 +0100

    pause menu added

--- Assets/Scripts/GameUIHandler.cs
+++ Assets/Scripts/GameUIHandler.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameUIHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject pauseMenu;
+    private bool paused;
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        if(!paused)
+        {
+            paused = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
